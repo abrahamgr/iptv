582ae999663fc321c369467df4d6939d2ee24071
Abraham Galindo <abraham.garo@gmail.com>
2026-02-13 17:07:09 -0600

    feat: add support for docker

--- app/db/client.server.ts
+++ app/db/client.server.ts
@@ -2,9 +2,11 @@ import { existsSync, mkdirSync } from 'node:fs'
 import { dirname, resolve } from 'node:path'
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 import * as schema from './schema.server'
 
 const DB_PATH = resolve('data/iptv.db')
+const MIGRATIONS_FOLDER = resolve('app/db/migrations')
 
 let db: ReturnType<typeof createDb>
 
@@ -16,7 +18,13 @@ function createDb() {
   const sqlite = new Database(DB_PATH)
   sqlite.pragma('journal_mode = WAL')
   sqlite.pragma('foreign_keys = ON')
-  return drizzle(sqlite, { schema })
+  const db = drizzle(sqlite, { schema })
+  // run only on prod, for dev migrations are run manually with `npm run db:migrate`
+  if (import.meta.env.PROD) {
+    console.log('MIGRATIONS_FOLDER', MIGRATIONS_FOLDER)
+    migrate(db, { migrationsFolder: MIGRATIONS_FOLDER })
+  }
+  return db
 }
 
 export function getDb() {
