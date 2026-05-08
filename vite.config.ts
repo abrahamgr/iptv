4cd558f10fcf117f5355cc097c15db33e8f24242
Abraham Galindo <abraham.garo@gmail.com>
2026-05-08 12:36:01 -0600

    chore: configure app base path to /tv for routing and asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // requires trailing slash for loading assets properly in production
+  base: `/tv${mode === 'production' ? '/' : ''}`,
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     port: 3000,
     host: true,
   },
-})
+}))
