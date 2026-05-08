335e1985f06cdd1d35b469f4ebdaad2f499242f3
Abraham Galindo <abraham.garo@gmail.com>
2026-05-08 16:43:26 -0600

    chore: upgrade React Routerand Vite, and remove vite-tsconfig-paths in favor of native resolve support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
   // requires trailing slash for loading assets properly in production
   base: `/tv${mode === 'production' ? '/' : ''}`,
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 3000,
     host: true,
